6c6ba4f195b1c3a5efeb32df9847527c76743af5
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-06-10 23:57:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+const taskForm = document.getElementById('create-task-form')
+const newTask = document.getElementById('new-task-description')
+const toDo = document.getElementById('tasks')
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    buildToDo()
+    form.reset()
+  })
+
 });
+
+
+function buildToDo() {
+  let ul = document.createElement('ul')
+  let btn = document.createElement('button')
+
+  btn.addEventListener('click', handleDelete)
+
+  btn.textContent = 'X'
+  ul.textContent = `${newTask.value}`
+
+  ul.appendChild(btn)
+  toDo.appendChild(ul)
+}
+
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
